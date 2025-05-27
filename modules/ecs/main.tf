20c65a225491239a67f75bc69f32f67d39d12262
Phillip Rolle <phil.rolle@gmail.com>
2025-05-27 11:07:50 -0400

    Adding in comments to ECS module

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,65 +1,70 @@
+# modules/ecs/main.tf
 # This module deploys an Nginx service on AWS ECS using Fargate.
 
+# CloudWatch Log Group for ECS logs - added for troubleshooting and monitoring
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
   name              = var.log_group_name
   retention_in_days = 7
 }
 
+# ECS Cluster - this is where the ECS service will run
 resource "aws_ecs_cluster" "main" {
   name = var.cluster_name
 }
-# This module deploys an Nginx service on AWS ECS using Fargate.
+
+# ECS Fargate Service for Nginx
 resource "aws_ecs_service" "nginx_service" {
   name            = "nginx-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.nginx_task.arn
-  desired_count   = var.desired_count
-  launch_type     = "FARGATE"
+  cluster         = aws_ecs_cluster.main.id # Reference the ECS cluster created above
+  task_definition = aws_ecs_task_definition.nginx_task.arn # Reference the task definition created below
+  desired_count   = var.desired_count # Number of desired tasks to run
+  launch_type     = "FARGATE" # Use Fargate launch type for serverless container management
 
   network_configuration {
-    subnets          = var.private_subnets
-    security_groups  = [var.ecs_sg_id]
-    assign_public_ip = true
+    subnets          = var.private_subnets # Use public subnets for the ECS tasks
+    security_groups  = [var.ecs_sg_id] # Security group for the ECS tasks
+    assign_public_ip = true # Assign public IPs to tasks for direct access
   }
 
   load_balancer {
-    target_group_arn = var.target_group_arn
-    container_name   = "nginx"
-    container_port   = 80
+    target_group_arn = var.target_group_arn # Target group ARN for the ALB
+    container_name   = "nginx" # Name of the container in the task definition
+    container_port   = 80 # Port on which the container listens
   }
 
   lifecycle {
-    ignore_changes = [task_definition] # Avoid replacement on minor image changes
+    ignore_changes = [task_definition] # Ignore changes to task definition to prevent unnecessary updates
   }
 }
 
+# ECS Task Definition for Nginx
 resource "aws_ecs_task_definition" "nginx_task" {
-  family                   = "nginx-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.cpu
-  memory                   = var.memory
-  execution_role_arn       = var.execution_role_arn
-  task_role_arn            = var.task_role_arn
+  family                   = "nginx-task" # Name of the task definition family
+  network_mode             = "awsvpc" # Use awsvpc network mode for Fargate tasks
+  requires_compatibilities = ["FARGATE"] # Specify that this task definition is compatible with Fargate
+  cpu                      = var.cpu # CPU units for the task
+  memory                   = var.memory   # Memory in MiB for the task
+  execution_role_arn       = var.execution_role_arn # IAM role for task execution
+  task_role_arn            = var.task_role_arn # IAM role for the task itself
 
   container_definitions = jsonencode([
     {
-      name      = "nginx"
-      image     = var.image_name  
-      essential = true
-      portMappings = [
+      name      = "nginx" # Name of the container
+      image     = var.image_name # Docker image for the container  
+      essential = true # Mark this container as essential for the task
+      portMappings = [ 
         {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
+          containerPort = 80 # Port on which the container listens
+          hostPort      = 80 # Port on the host (Fargate) to map to the container
+          protocol      = "tcp" # Protocol used for communication
         }
       ],
         logConfiguration = {
-            logDriver = "awslogs"
+            logDriver = "awslogs" # Use AWS CloudWatch Logs for logging
             options = {
-            "awslogs-group"         = var.log_group_name
-            "awslogs-region"        = var.region
-            "awslogs-stream-prefix" = "${var.cluster_name}-nginx"
+            "awslogs-group"         = var.log_group_name # CloudWatch Logs group for the container logs
+            "awslogs-region"        = var.region # AWS region for the logs
+            "awslogs-stream-prefix" = "${var.cluster_name}-nginx" # Prefix for log streams
             }
         }
     }
--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,69 +1,86 @@
+# modules/ecs/variables.tf
+# Input variables for ECS module to deploy an Nginx service using AWS Fargate
+# Note: This module assumes that the necessary IAM roles and policies for ECS and Fargate have been created in the iam module.
+
+# Name of the ECS cluster and task definition, along with other configurations for deploying an Nginx service on AWS Fargate.
 variable "cluster_name" {
   description = "ECS cluster name"
   type        = string
   default     = "nginx-demo"
 }
 
+# Name of the docker image to be used in the ECS task definition.
 variable "image_name" {
   description = "Docker image name for the ECS task"
   type        = string
-  default     = "nginx:alpine"
+  default     = "nginx:alpine" # Default to a lightweight Nginx image
 }
 
+# Cpu units and memory for the ECS task definition (e.g., 256 = 0.25 vCPU)
 variable "cpu" {
   description = "CPU units for task"
   type        = string
   default     = "256"
 }
 
+# Memory for the ECS task definition in MiB (e.g., 512 = 0.5 GiB)
 variable "memory" {
   description = "Memory for task (MiB)"
   type        = string
   default     = "512"
 }
 
+# ARN of the IAM role for ECS task execution and permissions
 variable "execution_role_arn" {
   description = "ECS Task execution role ARN"
   type        = string
 }
 
+# ARN of the IAM role for ECS tasks to assume
 variable "task_role_arn" {
   description = "ECS Task role ARN"
   type        = string
 }
 
+# Number of desired ECS tasks to run
 variable "desired_count" {
   description = "Number of ECS tasks to run"
   type        = number
   default     = 1
 }
 
+# List of private subnets where ECS tasks will run
 variable "private_subnets" {
   description = "List of subnet IDs for ECS tasks"
   type        = list(string)
 }
 
+# Security group ID for ECS tasks to control inbound/outbound traffic
 variable "ecs_sg_id" {
   description = "Security group ID for ECS tasks"
   type        = string
 }
 
+# ARN of the ALB listener to associate with the ECS service
 variable "listener_arn" {
   description = "ARN of the ALB listener"
   type        = string
 }
 
+# ARN of the ALB target group to register ECS tasks with
 variable "target_group_arn" {
   description = "ARN of the ALB target group"
   type        = string
 }
 
+# CloudWatch log group name for ECS task logs
 variable "log_group_name" {
   type        = string
   description = "CloudWatch log group name"
   default     = "nginx-ecs-demo-logs"
 }
 
+# AWS Region where the ECS resources will be deployed
 variable "region" {
   description = "AWS Region"
   type        = string
